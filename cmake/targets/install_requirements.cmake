8a4c4d53f11ef1c85e7a483ec8e7310f4a923702
Haowei Hsu <hwhsu1231@localizethedocs.org>
2026-05-21 18:13:23 +0800

    🐛 fix(script): bootstrap pip with ensurepip (GH-18)
    Ensure `pip` is available after installing `python` with Conda.
    Some local Conda setups may disable adding `pip` as a `python`
    dependency, and solver bugs can also skip `pip` installation.
    Run `python -m ensurepip` after `python` install to guarantee
    `pip` is present.
    
    Synced from: https://github.com/localizethedocs/ros2-docs-l10n/commit/c9c310428a4626d3d8f8e5830ac92513a52ae767

--- cmake/targets/install_requirements.cmake
+++ cmake/targets/install_requirements.cmake
@@ -241,6 +241,18 @@ message("")
 restore_cmake_message_indent()
 
 
+message(STATUS "Running 'python -m ensurepip' command to bootstrap the 'pip' installer...")
+remove_cmake_message_indent()
+message("")
+execute_process(
+    COMMAND ${Python_EXECUTABLE} -m ensurepip --default-pip
+    WORKING_DIRECTORY ${PROJ_OUT_REPO_DIR}
+    ECHO_OUTPUT_VARIABLE
+    ECHO_ERROR_VARIABLE)
+message("")
+restore_cmake_message_indent()
+
+
 if     (  ( VERSION MATCHES "^(main)$"        ) OR
           ( VERSION MATCHES "^(latest)$"      ) OR
           ( VERSION MATCHES "^([0-9]+)$"      AND
